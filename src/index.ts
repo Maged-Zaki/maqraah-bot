886da19a5966708e7ed6ccc38dce9fed87a7c94c
Maged Zaki <mohanad.zaki12300@gmail.com>
2025-12-09 20:07:34 +0200

    refactor: move if condition to the top

--- src/index.ts
+++ src/index.ts
@@ -68,24 +68,28 @@ client.once('clientReady', async () => {
 	scheduleReminder(client);
 
 	const channelId = process.env.CHANNEL_ID;
-	if (channelId) {
-		const channel = client.channels.cache.get(channelId);
-		if (channel && channel instanceof TextChannel) {
-			const permissions = channel.permissionsFor(client.user!);
-			if (permissions && permissions.has(PermissionsBitField.Flags.ViewChannel) && permissions.has(PermissionsBitField.Flags.SendMessages)) {
-				await channel.send(`
+
+	if (!channelId) {
+		console.error('CHANNEL_ID is not set in environment variables.');
+		return;
+	}
+
+	const channel = client.channels.cache.get(channelId);
+	if (channel instanceof TextChannel) {
+		const permissions = channel.permissionsFor(client.user!);
+		if (permissions && permissions.has(PermissionsBitField.Flags.ViewChannel) && permissions.has(PermissionsBitField.Flags.SendMessages)) {
+			await channel.send(`
 Hello! I am the Maqraah bot. I am here to help you track your daily Qur'an and Hadith reading.
 
-To get started, please use the \`/configure\` command to set up your preferences. You can configure multiple settings at once.
+To get started, please use the \`/configuration set\` command to set up your preferences. You can configure multiple settings at once.
 
 Once you have configured me, I will send you a daily reminder to read your Qur'an and Hadith.
 				`);
-			} else {
-				console.error(`Missing permissions to send messages in channel ${channelId}`);
-			}
 		} else {
-			console.log(`Channel ${channelId} not found in cache, skipping welcome message.`);
+			console.error(`Missing permissions to send messages in channel ${channelId}`);
 		}
+	} else {
+		console.log(`Channel ${channelId} not found in cache, skipping welcome message.`);
 	}
 });
 
